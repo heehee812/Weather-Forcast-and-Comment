225c9b5f3e2d44689c0c250af697ca9d5070380e
heehee812 <e.enyuu812@gmail.com>
2024-04-05 00:03:19 +0800

    add Main

---
+++ weather-mood/src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Main.css';
+
+export default class Main extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (<p>hi</p>);
+    }
+}
