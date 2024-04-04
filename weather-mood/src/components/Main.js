e754b9d39f839cc528495c2b211f8cf68faee2bb
heehee812 <e.enyuu812@gmail.com>
2024-04-05 00:32:43 +0800

    add navbar

--- weather-mood/src/components/Main.js
+++ weather-mood/src/components/Main.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 import './Main.css';
-
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
 import {
     Collapse,
     Navbar,
@@ -17,8 +21,36 @@ import {
 export default class Main extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            unit: 'metric',
+            navbarToggle: false,
+            searchText: ''
+        };
+        this.searchEl = null;
+
+        this.handleNavbarToggle = this.handleNavbarToggle.bind(this);
     }
     render() {
-        return (<Button>Hi</Button>);
+        return (
+            <div className='main bg-faded'>
+                <div className='container'></div>
+                <Navbar color="faded" light expand="md">
+                    <NavbarBrand className='text-info' href="/">WeatherMood</NavbarBrand>
+                    <NavbarToggler onClick={this.handleNavbarToggle} />
+                    <Collapse isOpen={this.state.navbarToggle} navbar>
+                        <Nav navbar>
+                            <NavItem>Today</NavItem>
+                            <NavItem>Forecast</NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+
+    handleNavbarToggle() {
+        this.setState((prevState, props) => ({
+            navbarToggle: !prevState.navbarToggle
+        }));
     }
 }
