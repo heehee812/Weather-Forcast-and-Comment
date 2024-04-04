b2a9924f5d6d188ab9ccf71aeb6ace241673a8a7
heehee812 <e.enyuu812@gmail.com>
2024-04-05 00:03:19 +0800

    add Main

--- weather-mood/src/index.js
+++ weather-mood/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <p>Hi</p>
-  </React.StrictMode>
-);
+import Main from './components/Main.js';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css'
+
+window.onload = function() {
+    ReactDOM.render(
+        <Main/>,
+        document.getElementById('root')
+    );
+};
