2cf47a81abc9ec005794ada6dd4f7737965dc3f4
heehee812 <e.enyuu812@gmail.com>
2024-04-05 00:17:58 +0800

    import reacctstrap

--- weather-mood/src/components/Main.js
+++ weather-mood/src/components/Main.js
@@ -2,11 +2,23 @@ import React from 'react';
 
 import './Main.css';
 
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Input,
+    Button
+} from 'reactstrap';
+
 export default class Main extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
-        return (<p>hi</p>);
+        return (<Button>Hi</Button>);
     }
 }
