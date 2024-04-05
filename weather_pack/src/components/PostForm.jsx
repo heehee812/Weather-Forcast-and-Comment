b26d14926eac820448e9b349c95135aca0d94c19
heehee812 <e.enyuu812@gmail.com>
2024-04-05 23:44:24 +0800

    input comment text

--- weather_pack/src/components/PostForm.jsx
+++ weather_pack/src/components/PostForm.jsx
@@ -26,13 +26,30 @@ export default class PostForm extends React.Component {
             moodToggle: false,
             mood: 'na'
         };
+
+        this.inputEl = null;
+        this.moodToggleEl = null;
+
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
     render() {
         const { inputValue, moodToggle, mood } = this.state;
         const inputDanger = this.state.inputDanger ? 'has-danger' : '';
         return (
-            <p>Postform</p>
+            <div className='post-form'>
+                <Alert color='info' className={`d-flex flex-column flex-sm-row justify-content-center ${inputDanger}`}>
+                <Input className='input' type='textarea' innerRef={el => {this.inputEl = el}} value={this.state.inputValue} onChange={this.handleInputChange} placeholder="What's on your mind?"></Input>
+                </Alert>
+            </div >
         );
+    }
+
+    handleInputChange(e) {
+        const text = e.target.value
+        this.setState({inputValue: text});
+        if (text) {
+            this.setState({inputDanger: false});
+        }
     }
 }
