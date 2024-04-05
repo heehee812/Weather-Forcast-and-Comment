708e0cf4887aad5262426c9fda022dd575241a8d
heehee812 <e.enyuu812@gmail.com>
2024-04-05 11:24:02 +0800

    update today

---
+++ weather-mood/src/components/Today.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Today.css';
+
+export default class Today extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <p>today is gonna be a good day to you!</p>
+        );
+    }
+}
