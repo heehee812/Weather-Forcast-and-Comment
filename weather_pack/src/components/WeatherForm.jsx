79d1a9ca8a4c8dc28662ccaaba73bc338023ae87
heehee812 <e.enyuu812@gmail.com>
2024-04-05 19:19:22 +0800

    handle submit

--- weather_pack/src/components/WeatherForm.jsx
+++ weather_pack/src/components/WeatherForm.jsx
@@ -31,14 +31,29 @@ export default class WeatherForm extends React.Component {
             tempToggle: false,
             unit: props.unit
         };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     render() {
         return (
             <div className='weather-form'>
-                <Form className='form-inline justify-content-center'></Form>
+                <Form className='form-inline justify-content-center' onSubmit={this.handleSubmit}>
+                    
+                </Form>
                 <p>Weather Formmmmmm</p>
             </div>
         );
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        this.inputEl.blur();
+        if (this.state.inputValue && this.state.inputValue.trim()) {
+            this.props.onQuery(this.state.inputValue, this.state.unit);
+        } else {
+            this.state.inputEl = this.props.city;
+        }
     }
 }
