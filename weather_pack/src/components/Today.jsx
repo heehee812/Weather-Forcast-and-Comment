831ca4d4afe882eeca022021ded7be72508811ae
heehee812 <e.enyuu812@gmail.com>
2024-04-05 22:44:02 +0800

    include weather form

--- weather_pack/src/components/Today.jsx
+++ weather_pack/src/components/Today.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Alert } from 'reactstrap';
+
+import WeatherForm from 'components/WeatherForm.jsx';
+import { getWeather } from 'api/open-weather-map.js';
+
+import './weather.css';
 import './Today.css';
 
 export default class Today extends React.Component {
@@ -10,27 +15,80 @@ export default class Today extends React.Component {
         onUnitChange: PropTypes.func
     };
 
+    static getInitWeatherState() {
+        return {
+            city: 'na',
+            code: -1,
+            group: 'na',
+            description: 'N/A',
+            temp: NaN
+        };
+    }
+
     constructor(props) {
         super(props);
 
         this.state = {
+            ...Today.getInitWeatherState(),
             loading: true,
             masking: true,
             postLoading: false,
             posts: []
         };
 
+        this.handleFormQuery = this.handleFormQuery.bind(this);
     }
+
     render() {
         const { unit } = this.props;
         return (
             <div className="today">
+                <div className='weather'>
+                    <WeatherForm city={this.state.city} unit={this.props.unit} onQuery={this.handleFormQuery}/>
+                </div>
                 <br></br>
                 <div className='posts'>
                     <Alert color='warning' className='loading'>Loading...</Alert>
                 </div>
-                <p>today is gonna be a good day to you!</p>
             </div>
         );
+    }
+
+    getWeather(city, unit) {
+        this.setState({
+            loading: true,
+            masking: true,
+            city: city // set city state immediately to prevent input text (in WeatherForm) from blinking;
+        }, () => { // called back after setState completes
+            getWeather(city, unit).then(weather => {
+                this.setState({
+                    ...weather,
+                    loading: false
+                }, () => this.notifyUnitChange(unit));
+            }).catch(err => {
+                console.error('Error getting weather', err);
+
+                this.setState({
+                    ...Today.getInitWeatherState(unit),
+                    loading: false
+                }, () => this.notifyUnitChange(unit));
+            });
+        });
+
+        setTimeout(() => {
+            this.setState({
+                masking: false
+            });
+        }, 600);
+    }
+
+    handleFormQuery(city, unit) {
+        this.getWeather(city, unit);
+    }
+
+    notifyUnitChange(unit) {
+        if (this.props.units !== unit) {
+            this.props.onUnitChange(unit);
+        }
     }
 }
